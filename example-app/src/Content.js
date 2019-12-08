936c0d7b2b8cd35c54a02e26c3ee8b3ba2700ac6
Daniel Sandoval <daniel@desandoval.net>
2019-12-08 15:37:58 -0500

    more suspense (no lists yet)

--- example-app/src/Content.js
+++ example-app/src/Content.js
@@ -1,72 +1,82 @@
-import React, {useTransition, useState} from 'react'
+import React, {
+  useState,
+  useTransition,
+  Suspense
+} from "react";
 import logo from './logo.svg';
 import './App.css';
+import { fetchProfileData } from "./fakeApi";
 
-function fetchSomeData() {
-  // simulates a 2 second data fetch
-  console.log('called')
-  return wrapPromise(new Promise(resolve => {
-    setTimeout(() => {
-      console.log('responded');
-      resolve({test: "value"})
-    }, 5000)
-  }))
+function getNextId(id) {
+  return id === 3 ? 0 : id + 1;
 }
 
-// simulate Relay
-function wrapPromise(promise) {
-  let status = "pending";
-  let result;
-  let suspender = promise.then(
-    r => {
-      status = "success";
-      result = r;
-    },
-    e => {
-      status = "error";
-      result = e;
-    }
-  );
-  return {
-    read() {
-      if (status === "pending") {
-        throw suspender;
-      } else if (status === "error") {
-        throw result;
-      } else if (status === "success") {
-        return result;
-      }
-    }
-  };
-}
+const initialResource = fetchProfileData(0);
 
 const Content = () => {
-    const [someState, updateSomeState] = useState({})
-    const [startTransition, isPending] = useTransition({ timeoutMs: 2000 });
-
-    console.log(someState);
-
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+  const [resource, setResource] = useState(
+    initialResource
+  );
+  const [
+    startTransition,
+    isPending
+  ] = useTransition({
+    timeoutMs: 500
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
           <button
-              disabled={ isPending }
-              onClick={ () => {
-                startTransition(() => {
-                  const data = fetchSomeData();
-                  updateSomeState(data);
-                })
-              } }
-            >
-            {isPending ? "Fetching" : "This will simulate a data fetch" }
-          </button>
-        </header>
-      </div>
-    )
+          disabled={isPending}
+          onClick={() => {
+            startTransition(() => {
+              const nextUserId = getNextId(
+                resource.userId
+              );
+              setResource(
+                fetchProfileData(nextUserId)
+              );
+            });
+          }}
+        >
+          Next
+        </button>
+        {isPending ? "Loading..." : null}
+        <ProfilePage resource={resource} />
+      </header>
+    </div>
+  );
 }
 
+function ProfilePage({ resource }) {
+  return (
+    <Suspense
+      fallback={<h1>Loading profile...</h1>}
+    >
+      <ProfileDetails resource={resource} />
+      <Suspense
+        fallback={<h1>Loading posts...</h1>}
+      >
+        <ProfileTimeline resource={resource} />
+      </Suspense>
+    </Suspense>
+  );
+}
+
+function ProfileDetails({ resource }) {
+  const user = resource.user.read();
+  return <h1>{user.name}</h1>;
+}
+
+function ProfileTimeline({ resource }) {
+  const posts = resource.posts.read();
+  return (
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.text}</li>
+      ))}
+    </ul>
+  );
+}
 export default Content;
